09bc0bfe0114f82cf0e0f2dcf3011fa7b13c4c30
Rizki22-017 <126841212+Rizki22-017@users.noreply.github.com>
2024-12-12 23:05:00 +0700

    tes

--- src/seller/myproduct-data/AddData.jsx
+++ src/seller/myproduct-data/AddData.jsx
@@ -1,12 +1,30 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import AOS from "aos";
+import axios from "axios";
+
 export const AddData = () => {
-    useEffect(() => {
-        AOS.init({
-          duration: 1000,
-          once: true,
-        });
-      }, []);
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    AOS.init({
+      duration: 1000,
+      once: true,
+    });
+
+    const fetchCategories = async () => {
+      try {
+        const response = await axios.get("http://localhost:3000/api/v1/category");
+        console.log(response.data);
+        setCategories(response.data.data); // Simpan data kategori
+      } catch (error) {
+        console.error("Error fetching categories:", error);
+      }
+    };
+
+    fetchCategories();
+  }, []);
+
+  
   return (
     <>
       <section className="adddata section">
@@ -43,13 +61,109 @@ export const AddData = () => {
             </div>
             <div className="mb-3">
               <label htmlFor="exampleFormControlInput1" className="form-label">
+                Location
+              </label>
+              <input
+                className="form-control"
+                type="text"
+                name="location"
+                placeholder="ex : Jl. Samudera, Padang, Indonesia"
+                aria-label="default input example"
+              />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="exampleFormControlInput1" className="form-label">
+                Lebar Kolam (ha)
+              </label>
+              <input
+                className="form-control"
+                type="decimal"
+                name="ponds_wide"
+                placeholder={1}
+                aria-label="default input example"
+              />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="exampleFormControlInput1" className="form-label">
+                Kapasitas Produksi (kg)
+              </label>
+              <input
+                className="form-control"
+                type="decimal"
+                name="production_capacity"
+                placeholder={30.0}
+                aria-label="default input example"
+              />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="exampleFormControlInput1" className="form-label">
+                Biaya Pakan
+              </label>
+              <input
+                className="form-control"
+                type="decimal"
+                name="feed_cost"
+                placeholder={1000000}
+                aria-label="default input example"
+              />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="exampleFormControlInput1" className="form-label">
+                Biaya Pekerja
+              </label>
+              <input
+                className="form-control"
+                type="decimal"
+                name="worker_cost"
+                placeholder={1000000}
+                aria-label="default input example"
+              />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="exampleFormControlInput1" className="form-label">
+                Biaya Perawatan
+              </label>
+              <input
+                className="form-control"
+                type="decimal"
+                name="maintenance_cost"
+                placeholder={1000000}
+                aria-label="default input example"
+              />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="exampleFormControlInput1" className="form-label">
+                Harga Jual Hasil Produk
+              </label>
+              <input
+                className="form-control"
+                type="decimal"
+                name="selling_price"
+                placeholder={1000000}
+                aria-label="default input example"
+              />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="exampleFormControlInput1" className="form-label">
                 Funds Managed
               </label>
               <input
                 className="form-control"
                 type="decimal"
-                name="promo"
+                name="funds_managed"
                 placeholder={80000}
+                aria-label="default input example"
+              />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="exampleFormControlInput1" className="form-label">
+                Estimasi Pendapatan
+              </label>
+              <input
+                className="form-control"
+                type="decimal"
+                name="estimated_income"
+                placeholder={800000}
                 aria-label="default input example"
               />
             </div>
@@ -60,7 +174,7 @@ export const AddData = () => {
               <input
                 className="form-control"
                 type="decimal"
-                name="harga"
+                name="margin"
                 placeholder={80000}
                 aria-label="default input example"
               />
@@ -87,8 +201,15 @@ export const AddData = () => {
                 aria-label="Small select example"
               >
                 <option value="">Pilih Kategori</option>
-                <option value="1">Sea Product</option>
-                <option value="2">Non-Sea Product</option>
+                {categories.length > 0 ? (
+                categories.map((category) => (
+                  <option key={category.id} value={category.id}>
+                    {category.category} {/* Ganti 'name' dengan field yang sesuai di API */}
+                  </option>
+                ))
+              ) : (
+                <option value="">No categories available</option>
+              )}
               </select>
             </div>
 
